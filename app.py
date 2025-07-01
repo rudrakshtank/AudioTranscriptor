e99dcd194afba8fa7c94bcaaa6c4d5ea50865832
Rudraksh Tank <24ucs052@lnmiit.ac.in>
2025-07-01 15:28:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import tempfile
 import os
 import shutil
-from moviepy import VideoFileClip
+from moviepy.editor import VideoFileClip
 import speech_recognition as sr
 import warnings
 warnings.filterwarnings("ignore")
